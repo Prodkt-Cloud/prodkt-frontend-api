ca73a4c42289fd3032b1bdbff859cfec0b10b9e6
Prodkt Team <technology@prodkt.cloud>
2022-11-02 10:16:33 -0600

    initial commit

---
+++ src/admin/app.tsx
@@ -0,0 +1,43 @@
+
+
+import favicon from './extensions/favicon.png';
+
+export default {
+  config: {
+
+    head: {
+      favicon: favicon,
+    },
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
