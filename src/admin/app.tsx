a5b3d11e2ca6cd3c441539e6661b05fd59820d60
Prodkt Team <technology@prodkt.cloud>
2022-11-02 11:48:24 -0600

    Translations in

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -14,13 +14,45 @@ export default {
     head: {
       favicon: favicon,
     },
+    theme: {
+      // overwrite light theme properties
+      light: {
+        colors: {
+          primary100: '#f6ecfc',
+          primary200: '#e0c1f4',
+          primary500: '#ac73e6',
+          primary600: '#9736e8',
+          primary700: '#8312d1',
+          danger700: '#b72b1a'
+        },
+      },
+
+      // overwrite dark theme properties
+      dark: {
+         // ...
+      }
+    },
+    // Extend the translations
+    translations: {
+      en: {
+        'Auth.form.email.label': 'Administrator Email',
+        'app.components.LeftMenu.navbrand.title': 'Prodkt',
+        'Auth.form.button.login.strapi': 'Login with Prodkt',
+        'Auth.form.welcome.title': 'Welcome to Prodkt',
+        'Auth.form.welcome.subtitle': 'Login to your Prodkt Administrator account',
+      },
+    },
+   // Disable video tutorials
+    tutorials: false,
+   // Disable notifications about new Strapi releases
+    notifications: { release: false },
     locales: [
       // 'ar',
-      // 'fr',
+      'fr',
       // 'cs',
-      // 'de',
+      'de',
       // 'dk',
-      // 'es',
+      'es',
       // 'he',
       // 'id',
       // 'it',
@@ -32,7 +64,7 @@ export default {
       // 'pl',
       // 'pt-BR',
       // 'pt',
-      // 'ru',
+      'ru',
       // 'sk',
       // 'sv',
       // 'th',
